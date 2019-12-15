c49c9e5b16280cef1ba4fce4e9559e83d40eed6e
msahincakir <55950833+msahincakir@users.noreply.github.com>
2019-12-16 02:01:28 +0300

    Supermarket
    Supermarket'in attribute'leri ve xml'leri update edildi. (class diagrama göre)
    Fonksiyonları  ile ilgili bi sıkıntı olduğunu düşünüyorum notlara yazdım ayrıntılı şekide.

--- Layout/Supermarket.cs
+++ Layout/Supermarket.cs
@@ -11,6 +11,7 @@ namespace FLOW.NET.Layout
     [XmlType("Supermarket")]
     public class Supermarket : Storage
     {
+        /*
         //ie486f18
         private RVGenerator loadTime;
 
@@ -28,6 +29,65 @@ namespace FLOW.NET.Layout
         {
             get { return this.loadTime; }
             set { this.loadTime = value; }
+        }
+        */ //bence bu ara komple olmamalı çünkü bu bypass olucağı assume edilerek yazılmış
+
+        //ie486fall19
+        private OrderList UnassignedOrderList; //dynamic
+        private BinList UnassignedBinList; //static!
+        private TransferTaskList TransferTasks; //dynamic
+        private TransporterList ReadyTransportersAtDock; //dynamic
+        private BinList EmptyBinList; //dynamic
+        private BinList AssignedBinList; //dynamic
+        private BinList ReadyBinList; //dynamic
+
+        [XmlIgnore()]
+        public OrderList UnassignedOrderList
+        {
+            get { return this.UnassignedOrderList; }
+            set { this.UnassignedOrderList = value; }
+        }
+
+        [XmlElement("UnassignedBinList")]
+        public BinList UnassignedBinList
+        {
+            get { return this.UnassignedBinList; }
+            set { this.UnassignedBinList = value; }
+        }
+
+        [XmlIgnore()]
+        public TransferTaskList TransferTasks
+        {
+            get { return this.TransferTasks; }
+            set { this.TransferTasks = value; }
+        }
+
+        [XmlIgnore()]
+        public TransporterList ReadyTransportersAtDock
+        {
+            get { return this.ReadyTransportersAtDock; }
+            set { this.ReadyTransportersAtDock = value; }
+        }
+
+        [XmlIgnore()]
+        public BinList EmptyBinList
+        {
+            get { return this.EmptyBinList; }
+            set { this.EmptyBinList = value; }
+        }
+
+        [XmlIgnore()]
+        public BinList AssignedBinList
+        {
+            get { return this.AssignedBinList; }
+            set { this.AssignedBinList = value; }
+        }
+
+        [XmlIgnore()]
+        public BinList ReadyBinList
+        {
+            get { return this.ReadyBinList; }
+            set { this.ReadyBinList = value; }
         }
     }
 }
